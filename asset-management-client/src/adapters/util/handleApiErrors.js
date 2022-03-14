2177c50f6fe1d263c28db26b98dcd96cc91658ca
Rafa Maciel <rafael.macielf@gmail.com>
2022-03-14 20:08:07 -0300

    update to only handle bad request errors code 400

--- asset-management-client/src/adapters/util/handleApiErrors.js
+++ asset-management-client/src/adapters/util/handleApiErrors.js
@@ -1,5 +1,5 @@
 function handleBadRequestError(error, cb) {
-    if (error && error.response && error.response.data ) {
+    if (error && error.response && error.response.status === 400 && error.response.data ) {
         var resp = error.response.data
         var newErrors = {}
         resp.errors.forEach(fieldErrorString => {
