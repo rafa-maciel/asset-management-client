219477a08479598e5be735d08f4a52dd183369d4
Rafael Maciel <rafael.macielf@gmail.com>
2022-03-16 15:44:47 -0300

    create new field types

--- asset-management-client/src/components/commons/forms/fields/FormFields.jsx
+++ asset-management-client/src/components/commons/forms/fields/FormFields.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { TextField } from '@material-ui/core'
+import React, { useEffect } from 'react'
+import { FormControl, FormHelperText, InputLabel, MenuItem, Select, TextField } from '@material-ui/core'
 import { Controller } from 'react-hook-form'
 
 
@@ -22,4 +22,50 @@ function FormTextField({ name, label, control}) {
     )
 }
 
-export { FormTextField }
+function FormNumberField({ name, label, control}) {
+    return (
+        <Controller
+            name={ name }
+            control={ control }
+            render={ ({ field: { onChange, value}, fieldState: { error } }) => (
+                <TextField
+                    InputLabelProps={{ shrink: true }}
+                    type="number"
+                    label={ label }
+                    value={ value }
+                    onChange={onChange}
+                    error={ !!error }
+                    helperText={ error ? error.message : null }
+                    fullWidth />
+            )}
+            />
+    )
+}
+
+function FormSelectField({ name, label, control, children }) {
+    
+    return (
+        <Controller
+            name={ name }
+            control={ control }
+            render={ ({ field: { onChange, value}, fieldState: { error } }) => (
+                <FormControl fullWidth >
+                    <InputLabel id={name + "-label"} shrink>{ label }</InputLabel>
+                    <Select
+                        labelId={name + "-label"}
+                        value={ value }
+                        onChange={ onChange }
+                        fullWidth
+                        defaultValue=""
+                        error={ !!error }>
+                        
+                        { children }
+                    </Select>
+                    <FormHelperText>{ error ? error.message : null }</FormHelperText>
+                </FormControl>
+            )}
+            />
+    )
+}
+
+export { FormTextField, FormNumberField, FormSelectField }
